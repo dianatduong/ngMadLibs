bdcc7cd0bda26ac210a37fa8659fa16958d877dd
Diana Duong <dduong013@gmail.com>
2016-06-08 16:01:03 -0500

    removed ng-controller and ng-repeat

--- js/script.js
+++ js/script.js
@@ -1,17 +1,2 @@
 
-angular.module('myApp', []).
-controller('myCtrl', function($scope) {
-   var words = [ 
-    {maleName: '',
-    jobTitle: '',
-    tediousTask: '',
-    dirtyTask: '',
-    celebrity: '',
-    uselessSkill: '',
-    obnoxiousCelebrity: '',
-    hugeNumber: '',
-    adjective: ''
-   }
-   ];
-   $scope.words = words;
-});
+angular.module('myApp', [])
