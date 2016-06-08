5ef8c2c7c15fafe8c479fd0941d818bf926c5be7
Diana Duong <dduong013@gmail.com>
2016-06-08 18:08:50 -0500

    added gender scope

--- js/script.js
+++ js/script.js
@@ -1,6 +1,45 @@
 
 angular.module('myApp', []) 
   .controller('myCtrl', function($scope) {
-    
+    // var maleName = [{
+    //   he: 'he',
+    //   him: 'him',
+    //   his: 'his'
+    // }]
+    // $scope.maleName = maleName;
+
+    // var femaleName = [{
+    //   she: 'she',
+    //   her: 'her'
+    // }]
+    // $scope.femaleName = femaleName;
+
+    $scope.gender = "female";
+
+    $scope.firstPersonSubjective = { 
+      female: 'she',
+      male: 'he'
+    }
+
+    $scope.firstPersonObjective = {
+      female: 'her',
+      male: 'him'
+    }
 
   });
+
+
+
+// //   var words = [ 
+//     {maleName: '',
+//     jobTitle: '',
+//     tediousTask: '',
+//     dirtyTask: '',
+//     celebrity: '',
+//     uselessSkill: '',
+//     obnoxiousCelebrity: '',
+//     hugeNumber: '',
+//     adjective: ''
+//    }
+//    ];
+//    $scope.words = words;
