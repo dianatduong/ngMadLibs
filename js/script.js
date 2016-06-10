4823dfff578774c52abb575c54e22cd85983141e
Diana Duong <dduong013@gmail.com>
2016-06-10 16:09:42 -0500

    added startover button

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ var app = angular.module('myApp', ['ngMessages'])
   app.controller('myCtrl', function() {
 
     var vm = this;
-
     //change gender
     // sets the radio button input to female on default
     vm.gender = "female"
@@ -15,14 +14,16 @@ var app = angular.module('myApp', ['ngMessages'])
       male: 'his'
     }
 
+
+
     //hides results section
     vm.results = false;
     vm.vm= {}
     vm.submittedData = []
 
-      vm.submitForm = function(isValid){
+    vm.submitForm = function(isValid){
      
-       if (isValid) {
+      if (isValid) {
          vm.submittedData.push(vm.vm)
          vm.vm = {}
          //displays results section
@@ -32,6 +33,25 @@ var app = angular.module('myApp', ['ngMessages'])
         vm.submitted = true;
        }
      }
+
+
+
+    vm.startover = function(){
+      vm.name = "";
+      vm.jobTitle = "";
+      vm.tediousTask = "";
+      vm.dirtyTask = "";
+      vm.celebrity = "";
+      vm.uselessSkill = "";
+      vm.obnoxiousCelebrity = "";
+      vm.hugeNumber = "";
+      vm.adjective = "";
+      //hides results section
+      vm.results = false;
+    }
+
+
+
   })
 
 
