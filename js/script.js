36dd24f912176fadda7b6d1505bb4c9916f1de04
Diana Duong <dduong013@gmail.com>
2016-06-10 15:50:16 -0500

    added form validations

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+var app = angular.module('myApp', ['ngMessages']) 
+  app.controller('myCtrl', function() {
 
-angular.module('myApp', []) 
-  .controller('myCtrl', function() {
     var vm = this;
+
+    //change gender
     // sets the radio button input to female on default
     vm.gender = "female"
     vm.firstPersonSubjective = { 
@@ -10,7 +12,27 @@ angular.module('myApp', [])
     }
     vm.firstPersonObjective = {
       female: 'her',
-      male: 'him'
+      male: 'his'
     }
+
+    //hides results section
+    vm.results = false;
+    vm.vm= {}
+    vm.submittedData = []
+
+      vm.submitForm = function(isValid){
+     
+       if (isValid) {
+         vm.submittedData.push(vm.vm)
+         vm.vm = {}
+         //displays results section
+         vm.results = true;
+      } 
+      else {
+        vm.submitted = true;
+       }
+     }
   })
 
+
+  
